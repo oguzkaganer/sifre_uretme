0b9f35478ca919990a8a3d6dc57c380da67767eb
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-09-29 00:11:45 +0300

    Add project files.

---
+++ sifre_uretme/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char[] karakter = new char[26];
+int i = 0;
+
+for (char k = 'a'; k <= 'z' ; k++)
+{
+    karakter[i] = k;
+    i++;
+}
+
+i = 0;
+while (i<=karakter.Length-1)
+{
+    Console.Write(karakter[i]+" ");
+    i++;
+}
+
+Console.WriteLine();
+
+string sifre = "";
+int dizisayaci = 0;
+
+Random uret = new Random();
+
+for (i = 0; i <= 9; i++)
+{
+    dizisayaci = uret.Next(0, 25);
+    sifre = sifre + karakter[dizisayaci];
+}
+
+Console.WriteLine("Üretilen şifre : " +sifre);
+
+Console.ReadKey();
